f9392483db0d050e564b99c21df5af93d3ea8bd1
Niaz Ahamed <dr.niazahamed24@gmail.com>
2023-07-23 04:37:43 -0700

    Made website run on localhost and removed an external node module which breached project requirements

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const cors = require('cors');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(cors());
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 /**
  * /bmi:
